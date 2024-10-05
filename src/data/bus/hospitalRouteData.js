d65da52769abd292f8c1bd94b72f7e49666842f6
NotJerwee <jerrylin2488@gmail.com>
2024-10-05 13:34:55 -0400

    Initial commit

---
+++ src/data/bus/hospitalRouteData.js
@@ -0,0 +1,19 @@
+import L from 'leaflet';
+
+export const hospitalRouteData = {
+    hospitalRouteStopIcon: new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-purple.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41],
+    }),
+        
+    busStops: [
+
+    ],
+
+    busRoute: [
+        
+    ]
+}
