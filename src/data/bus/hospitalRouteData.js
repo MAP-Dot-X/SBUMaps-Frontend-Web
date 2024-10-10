c6701b4c77246bd5df2a4e54796ebdebd8447349
NotJerwee <jerrylin2488@gmail.com>
2024-10-10 13:48:26 -0400

    Add hospital/chapin bus routes

--- src/data/bus/hospitalRouteData.js
+++ src/data/bus/hospitalRouteData.js
@@ -11,9 +11,37 @@ export const hospitalRouteData = {
 
     busStops: [
         { name: 'SAC', position: [40.91401431367258, -73.12468099152272] },
+        { name: 'Roosevelt West', position: [40.91202250727283, -73.12804710194747] },
+        { name: 'Tabler Steps West', position: [40.911084504927956, -73.12646123513507] },
+        { name: 'Roth/Tabler West', position: [40.91036665662295, -73.12457431596597] },
+        { name: 'South Campus West', position: [40.904759187338726, -73.1217362526996] },
+        { name: 'Child Care', position: [40.89823539485135, -73.12810625953261] },
+        { name: 'Lot 40', position: [40.89688182178211, -73.12666708166921] },
+        { name: 'Chapin', position: [40.90838285875693, -73.11159511736321] },
+        { name: 'Point of Woods', position: [40.913936298767986, -73.11472621986402] },
+        { name: 'Administration Building', position: [40.914139223413265, -73.11921333945378] },
+        { name: 'Life Sciences West', position: [40.910969924136715, -73.11916997593116] },
+        { name: 'Roth/Tabler East', position: [40.91037388510646, -73.12411823511705] },
+        { name: 'Tabler Steps East', position: [40.911054734673876, -73.12599884441968] },
+        { name: 'Roosevelt East', position: [40.911915058649925, -73.12762103836138] },
+        { name: 'Circle Road', position: [40.91308894957686, -73.12906926717163] },
     ],
 
     busRoute: [
-        // Define route coordinates here if needed
+        [40.91401431367258, -73.12468099152272], // SAC
+        [40.91202250727283, -73.12804710194747], // Roosevelt West
+        [40.911084504927956, -73.12646123513507], // Tabler Steps West
+        [40.91036665662295, -73.12457431596597], // Roth/Tabler West
+        [40.904759187338726, -73.1217362526996], // South Campus West
+        [40.89823539485135, -73.12810625953261], // Child Care
+        [40.89688182178211, -73.12666708166921], // Lot 40
+        [40.90838285875693, -73.11159511736321], // Chapin
+        [40.913936298767986, -73.11472621986402], // Point of Woods
+        [40.914139223413265, -73.11921333945378], // Administration Building
+        [40.910969924136715, -73.11916997593116], // Life Sciences West
+        [40.91037388510646, -73.12411823511705], // Roth/Tabler East
+        [40.911054734673876, -73.12599884441968], // Tabler Steps East
+        [40.911915058649925, -73.12762103836138], // Roosevelt East
+        [40.91308894957686, -73.12906926717163], // Circle Road
     ]
 };
